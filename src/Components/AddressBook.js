e4a27b1ba0015cab9e72705b56e985a16913dd0f
Akshaya <aramanar@ford.com>
2023-01-23 18:51:11 -0500

    addressbook render

--- src/Components/AddressBook.js
+++ src/Components/AddressBook.js
@@ -19,16 +19,15 @@ function AddressBook() {
 
   return (
     <div className="App">
-      <h1>Address Book</h1>
+      <h1 class="text-5xl">Address Book</h1>
+        <br/>
       { showError ? <h2>Something's wrong at the server. Please try later</h2> : null}
-        <div class={"grid grid-cols-4 gap-4"}>
+        <div class={"grid grid-cols-5 gap-5"}>
         
             {users.map((user,index)=> {
                 return (
-                    <div>
-                        <img
-                            className="w-full"
-                            src={user.picture.medium} alt=""/>
+                    <div className={"bg-sky-500/10"}>
+                        <img src={user.picture.medium} alt=""/>
                         <div className="px-6 py-4">
                             <div className="font-bold text-xl mb-2">{user.name.title} {user.name.first} {user.name.last}</div>
                             <p className="text-gray-700 text-base">{user.gender}  ( {user.dob.age} )</p>
